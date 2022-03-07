47e263baf54c70cb8272300f7c43e34235753b50
dmmlakj <dmmlakj@gmail.com>
2022-03-08 00:24:01 +0530

    Initial commit

---
+++ src/main/java/com/amedtraining2/adecloud/AdecloudApplication.java
@@ -0,0 +1,13 @@
+package com.amedtraining2.adecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdecloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdecloudApplication.class, args);
+	}
+
+}
